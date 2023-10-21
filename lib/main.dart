0f4051bcb6b0ff3b59eb4b0b7a36f44d6998ca89
Akeel Nathom <akeeel767@gmail.com>
2023-10-21 11:06:31 +0300

    connect with firbase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:coffy_application/screens/Auth%20Screens/signUpScreen.dart';
-import 'package:coffy_application/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
